0172a8f25ecb2c76f14f1dd704c5372a36757e86
Toan Lam <toanlam01@gmail.com>
2022-10-19 21:52:07 -0700

    change build command

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,17 +1,10 @@
 /** @format */
 
 module.exports = {
-  webpackFinal: async (config) => {
-    // find web-components rule for extra transpilation
-    const webComponentsRule = config.module.rules.find(
-      (rule) =>
-        rule.use && rule.use.options && rule.use.options.babelrc === false
-    );
-    // add your own `my-library`
-    webComponentsRule.test.push(
-      new RegExp(`node_modules(\\/|\\\\)my-library(.*)\\.js$`)
-    );
-
-    return config;
-  },
+  stories: [
+    "../stories/**/*.stories.mdx",
+    "../stories/**/*.stories.@(js|jsx|ts|tsx)",
+  ],
+  addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
+  framework: "@storybook/web-components",
 };
